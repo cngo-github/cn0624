efa64378a0a07f7a3847156240b8e48a1247ef48
Chuong Ngo <cngo.github.com>
2024-06-15 06:49:03 -0400

    Cleaned code.

--- src/main/java/org/example/persistence/cache/CacheEntryNotFound.java
+++ src/main/java/org/example/persistence/cache/CacheEntryNotFound.java
@@ -1,6 +1,9 @@
 package org.example.persistence.cache;
 
+import java.io.Serial;
+
 public class CacheEntryNotFound extends Exception {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public CacheEntryNotFound(String message) {
